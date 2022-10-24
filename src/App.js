8ee97b8ad53938436f50ca9e367a4b27c70b2ab0
bimbo-ade <bimboadeyemi65@gmail.com>
2022-10-24 13:55:59 +0100

    installed styled components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Div } from "./Home.style";
+const Home = () => {
+  return <Div>Home</Div>;
+};
+
+export default Home;
