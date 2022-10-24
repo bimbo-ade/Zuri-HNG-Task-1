d002b79456d88cb45298cfca7b28d5a0b33e7e76
bimbo-ade <bimboadeyemi65@gmail.com>
2022-10-24 16:07:42 +0100

    links display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,18 +6,69 @@ import { Div } from "./Home.style";
 const Home = () => {
   return (
     <Div>
-      <div className="container">
-        <img src={share} alt="" className="share-icon" />
-        <div className="profile-container">
-          <div className="img-containter">
-            <img src={profile} alt="" width={50} />
-          </div>
-          <h4>Adeyemi Ab</h4>
+      <img src={share} alt="" className="share-icon" />
+      <div className="profile-container">
+        <div className="img-containter">
+          <img src={profile} alt="" width={50} />
         </div>
-        <div className="links"></div>
-        <div className="social-icon"></div>
-        <div className="footer"></div>
+        <h4>Adeyemi Ab</h4>
       </div>
+      <div className="link-container">
+        <a
+          href="https://twitter.com/_dami_sarah_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Twitter Link
+        </a>
+
+        <a
+          href="https://training.zuri.team/"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="btn__zuri"
+        >
+          Zuri Team
+        </a>
+
+        <a
+          href="http://books.zuri.team"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="books"
+        >
+          Zuri Books
+        </a>
+
+        <a
+          href="https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="book__python"
+        >
+          Python Books
+        </a>
+
+        <a
+          href="https://background.zuri.team"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="pitch"
+        >
+          Background Check for Coders
+        </a>
+
+        <a
+          href="https://books.zuri.team/design-rules"
+          target="_blank"
+          rel="noopener noreferrer"
+          id="book__design"
+        >
+          Design Books
+        </a>
+      </div>
+      <div className="social-icon"></div>
+      <div className="footer"></div>
     </Div>
   );
 };
--- src/components/Home/Home.style.js
+++ src/components/Home/Home.style.js
@@ -2,17 +2,18 @@ import styled from "styled-components";
 
 export const Div = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   position: relative;
-  width: 90%;
+  width: 80%;
   margin: 0 auto 0;
 
   .share-icon {
     position: absolute;
     width: 40px;
     height: 40px;
-    right: 280px;
+    right: 230px;
     top: 50px;
   }
 
@@ -31,6 +32,32 @@ export const Div = styled.div`
     font-size: 20px;
     line-height: 30px;
     text-align: center;
+    color: #101828;
+  }
+
+  .link-container {
+    display: flex;
+    width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    margin-top: 24px;
+    height: 56px;
+    box-sizing: border-box;
+    background-color: #eaecf0;
+    border: 1px solid #eaecf0;
+    border-radius: 8px;
+    font-weight: 600;
+    font-size: 18px;
+    line-height: 28px;
+    text-decoration: none;
     color: #101828;
   }
 `;
