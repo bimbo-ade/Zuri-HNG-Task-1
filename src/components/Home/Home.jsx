d68d6de708510bf785738755c154469d8e12879c
bimbo-ade <bimboadeyemi65@gmail.com>
2022-10-24 15:03:14 +0100

    profile display

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import profile from "../../assets/images/profile.jpg";
+
+import share from "../../assets/images/share.png";
 import { Div } from "./Home.style";
 const Home = () => {
-  return <Div>Home</Div>;
+  return (
+    <Div>
+      <div className="container">
+        <img src={share} alt="" className="share-icon" />
+        <div className="profile-container">
+          <div className="img-containter">
+            <img src={profile} alt="" width={50} />
+          </div>
+          <h4>Adeyemi Ab</h4>
+        </div>
+        <div className="links"></div>
+        <div className="social-icon"></div>
+        <div className="footer"></div>
+      </div>
+    </Div>
+  );
 };
 
 export default Home;
--- src/components/Home/Home.style.js
+++ src/components/Home/Home.style.js
@@ -1,3 +1,36 @@
 import styled from "styled-components";
 
-export const Div = styled.div``;
+export const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 90%;
+  margin: 0 auto 0;
+
+  .share-icon {
+    position: absolute;
+    width: 40px;
+    height: 40px;
+    right: 280px;
+    top: 50px;
+  }
+
+  .img-containter {
+    img {
+      width: 88px;
+      height: 88px;
+      border-radius: 50%;
+      margin-top: 64px;
+    }
+  }
+
+  h4 {
+    margin-top: 24px;
+    font-weight: 700;
+    font-size: 20px;
+    line-height: 30px;
+    text-align: center;
+    color: #101828;
+  }
+`;
